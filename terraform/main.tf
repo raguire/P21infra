b6f6a53f49e2c92bd3f9b679f4aee8470e23fc9d
Ricardo <ricardo.aguirre.ruiz@gmail.com>
2022-01-19 18:09:07 -0600

    remove blank lines from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,32 +21,19 @@ provider "azurerm" {
 }
 
 variable "prefix" {
-
   default = "webnp"
-
 }
 
 resource "azurerm_resource_group" "rg" {
-
   name     = "p21-eus2-np"
-
   location = "eastus2"
-
 }
-
-
 
 resource "azurerm_network_security_group" "nsg" {
-
   name                = "p21-nsg"
-
   location            = azurerm_resource_group.rg.location
-
   resource_group_name = azurerm_resource_group.rg.name
-
 }
-
-
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "p21-eus2-vnet"
@@ -55,9 +42,7 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   #dns_servers         = ["10.0.0.4", "10.0.0.5"]
   tags = {
-
     environment = "Non Prod"
-
   }
 
 }
@@ -68,8 +53,6 @@ resource "azurerm_subnet" "p21web" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-
-
 
 resource "azurerm_public_ip" "p21pip" {
   name                = "p21-web-pip"
@@ -93,78 +76,43 @@ resource "azurerm_network_interface" "mainweb" {
   }
 }
 
-
-
 resource "azurerm_virtual_machine" "mainvmweb" {
-
   name                  = "${var.prefix}-vm"
-
   location              = azurerm_resource_group.rg.location
-
   resource_group_name   = azurerm_resource_group.rg.name
-
   network_interface_ids = [azurerm_network_interface.mainweb.id]
-
   vm_size               = "Standard_DS1_v2"
-
-
-
   # Uncomment this line to delete the OS disk automatically when deleting the VM
-
   # delete_os_disk_on_termination = true
-
-
-
   # Uncomment this line to delete the data disks automatically when deleting the VM
-
   # delete_data_disks_on_termination = true
 
-
-
   storage_image_reference {
-
     publisher = "Canonical"
-
     offer     = "UbuntuServer"
-
     sku       = "16.04-LTS"
-
     version   = "latest"
-
   }
 
   storage_os_disk {
-
     name              = "myosdisk1"
-
     caching           = "ReadWrite"
-
     create_option     = "FromImage"
-
     managed_disk_type = "Standard_LRS"
-
   }
 
   os_profile {
-
     computer_name  = "p21web1"
-
     admin_username = "testadmin"
-
     admin_password = "Password1234!"
-
   }
 
   os_profile_linux_config {
-
     disable_password_authentication = false
-
   }
 
   tags = {
-
     environment = "Non Prod"
-
   }
 
 }
